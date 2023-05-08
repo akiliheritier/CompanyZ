44261e4638d8b8bcd590622dcfccd1fe516389a1
akili heritier <heritierakili@gmail.com>
2023-05-08 22:01:43 +0300

    This the application database

---
+++ company_z_db.sql
@@ -0,0 +1,417 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2023 at 09:00 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `company_z_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `apps_countries`
+--
+
+CREATE TABLE `apps_countries` (
+  `id` int(11) NOT NULL,
+  `country_code` varchar(2) NOT NULL DEFAULT '',
+  `country_name` varchar(100) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `apps_countries`
+--
+
+INSERT INTO `apps_countries` (`id`, `country_code`, `country_name`) VALUES
+(1, 'AF', 'Afghanistan'),
+(2, 'AL', 'Albania'),
+(3, 'DZ', 'Algeria'),
+(4, 'AS', 'American Samoa'),
+(5, 'AD', 'Andorra'),
+(6, 'AO', 'Angola'),
+(7, 'AI', 'Anguilla'),
+(8, 'AQ', 'Antarctica'),
+(9, 'AG', 'Antigua and Barbuda'),
+(10, 'AR', 'Argentina'),
+(11, 'AM', 'Armenia'),
+(12, 'AW', 'Aruba'),
+(13, 'AU', 'Australia'),
+(14, 'AT', 'Austria'),
+(15, 'AZ', 'Azerbaijan'),
+(16, 'BS', 'Bahamas'),
+(17, 'BH', 'Bahrain'),
+(18, 'BD', 'Bangladesh'),
+(19, 'BB', 'Barbados'),
+(20, 'BY', 'Belarus'),
+(21, 'BE', 'Belgium'),
+(22, 'BZ', 'Belize'),
+(23, 'BJ', 'Benin'),
+(24, 'BM', 'Bermuda'),
+(25, 'BT', 'Bhutan'),
+(26, 'BO', 'Bolivia'),
+(27, 'BA', 'Bosnia and Herzegovina'),
+(28, 'BW', 'Botswana'),
+(29, 'BV', 'Bouvet Island'),
+(30, 'BR', 'Brazil'),
+(31, 'IO', 'British Indian Ocean Territory'),
+(32, 'BN', 'Brunei Darussalam'),
+(33, 'BG', 'Bulgaria'),
+(34, 'BF', 'Burkina Faso'),
+(35, 'BI', 'Burundi'),
+(36, 'KH', 'Cambodia'),
+(37, 'CM', 'Cameroon'),
+(38, 'CA', 'Canada'),
+(39, 'CV', 'Cape Verde'),
+(40, 'KY', 'Cayman Islands'),
+(41, 'CF', 'Central African Republic'),
+(42, 'TD', 'Chad'),
+(43, 'CL', 'Chile'),
+(44, 'CN', 'China'),
+(45, 'CX', 'Christmas Island'),
+(46, 'CC', 'Cocos (Keeling) Islands'),
+(47, 'CO', 'Colombia'),
+(48, 'KM', 'Comoros'),
+(49, 'CD', 'Democratic Republic of the Congo'),
+(50, 'CG', 'Republic of Congo'),
+(51, 'CK', 'Cook Islands'),
+(52, 'CR', 'Costa Rica'),
+(53, 'HR', 'Croatia (Hrvatska)'),
+(54, 'CU', 'Cuba'),
+(55, 'CY', 'Cyprus'),
+(56, 'CZ', 'Czech Republic'),
+(57, 'DK', 'Denmark'),
+(58, 'DJ', 'Djibouti'),
+(59, 'DM', 'Dominica'),
+(60, 'DO', 'Dominican Republic'),
+(61, 'TL', 'East Timor'),
+(62, 'EC', 'Ecuador'),
+(63, 'EG', 'Egypt'),
+(64, 'SV', 'El Salvador'),
+(65, 'GQ', 'Equatorial Guinea'),
+(66, 'ER', 'Eritrea'),
+(67, 'EE', 'Estonia'),
+(68, 'ET', 'Ethiopia'),
+(69, 'FK', 'Falkland Islands (Malvinas)'),
+(70, 'FO', 'Faroe Islands'),
+(71, 'FJ', 'Fiji'),
+(72, 'FI', 'Finland'),
+(73, 'FR', 'France'),
+(74, 'FX', 'France, Metropolitan'),
+(75, 'GF', 'French Guiana'),
+(76, 'PF', 'French Polynesia'),
+(77, 'TF', 'French Southern Territories'),
+(78, 'GA', 'Gabon'),
+(79, 'GM', 'Gambia'),
+(80, 'GE', 'Georgia'),
+(81, 'DE', 'Germany'),
+(82, 'GH', 'Ghana'),
+(83, 'GI', 'Gibraltar'),
+(84, 'GG', 'Guernsey'),
+(85, 'GR', 'Greece'),
+(86, 'GL', 'Greenland'),
+(87, 'GD', 'Grenada'),
+(88, 'GP', 'Guadeloupe'),
+(89, 'GU', 'Guam'),
+(90, 'GT', 'Guatemala'),
+(91, 'GN', 'Guinea'),
+(92, 'GW', 'Guinea-Bissau'),
+(93, 'GY', 'Guyana'),
+(94, 'HT', 'Haiti'),
+(95, 'HM', 'Heard and Mc Donald Islands'),
+(96, 'HN', 'Honduras'),
+(97, 'HK', 'Hong Kong'),
+(98, 'HU', 'Hungary'),
+(99, 'IS', 'Iceland'),
+(100, 'IN', 'India'),
+(101, 'IM', 'Isle of Man'),
+(102, 'ID', 'Indonesia'),
+(103, 'IR', 'Iran (Islamic Republic of)'),
+(104, 'IQ', 'Iraq'),
+(105, 'IE', 'Ireland'),
+(106, 'IL', 'Israel'),
+(107, 'IT', 'Italy'),
+(108, 'CI', 'Ivory Coast'),
+(109, 'JE', 'Jersey'),
+(110, 'JM', 'Jamaica'),
+(111, 'JP', 'Japan'),
+(112, 'JO', 'Jordan'),
+(113, 'KZ', 'Kazakhstan'),
+(114, 'KE', 'Kenya'),
+(115, 'KI', 'Kiribati'),
+(116, 'KP', 'Korea, Democratic People\'s Republic of'),
+(117, 'KR', 'Korea, Republic of'),
+(118, 'XK', 'Kosovo'),
+(119, 'KW', 'Kuwait'),
+(120, 'KG', 'Kyrgyzstan'),
+(121, 'LA', 'Lao People\'s Democratic Republic'),
+(122, 'LV', 'Latvia'),
+(123, 'LB', 'Lebanon'),
+(124, 'LS', 'Lesotho'),
+(125, 'LR', 'Liberia'),
+(126, 'LY', 'Libyan Arab Jamahiriya'),
+(127, 'LI', 'Liechtenstein'),
+(128, 'LT', 'Lithuania'),
+(129, 'LU', 'Luxembourg'),
+(130, 'MO', 'Macau'),
+(131, 'MK', 'North Macedonia'),
+(132, 'MG', 'Madagascar'),
+(133, 'MW', 'Malawi'),
+(134, 'MY', 'Malaysia'),
+(135, 'MV', 'Maldives'),
+(136, 'ML', 'Mali'),
+(137, 'MT', 'Malta'),
+(138, 'MH', 'Marshall Islands'),
+(139, 'MQ', 'Martinique'),
+(140, 'MR', 'Mauritania'),
+(141, 'MU', 'Mauritius'),
+(142, 'YT', 'Mayotte'),
+(143, 'MX', 'Mexico'),
+(144, 'FM', 'Micronesia, Federated States of'),
+(145, 'MD', 'Moldova, Republic of'),
+(146, 'MC', 'Monaco'),
+(147, 'MN', 'Mongolia'),
+(148, 'ME', 'Montenegro'),
+(149, 'MS', 'Montserrat'),
+(150, 'MA', 'Morocco'),
+(151, 'MZ', 'Mozambique'),
+(152, 'MM', 'Myanmar'),
+(153, 'NA', 'Namibia'),
+(154, 'NR', 'Nauru'),
+(155, 'NP', 'Nepal'),
+(156, 'NL', 'Netherlands'),
+(157, 'AN', 'Netherlands Antilles'),
+(158, 'NC', 'New Caledonia'),
+(159, 'NZ', 'New Zealand'),
+(160, 'NI', 'Nicaragua'),
+(161, 'NE', 'Niger'),
+(162, 'NG', 'Nigeria'),
+(163, 'NU', 'Niue'),
+(164, 'NF', 'Norfolk Island'),
+(165, 'MP', 'Northern Mariana Islands'),
+(166, 'NO', 'Norway'),
+(167, 'OM', 'Oman'),
+(168, 'PK', 'Pakistan'),
+(169, 'PW', 'Palau'),
+(170, 'PS', 'Palestine'),
+(171, 'PA', 'Panama'),
+(172, 'PG', 'Papua New Guinea'),
+(173, 'PY', 'Paraguay'),
+(174, 'PE', 'Peru'),
+(175, 'PH', 'Philippines'),
+(176, 'PN', 'Pitcairn'),
+(177, 'PL', 'Poland'),
+(178, 'PT', 'Portugal'),
+(179, 'PR', 'Puerto Rico'),
+(180, 'QA', 'Qatar'),
+(181, 'RE', 'Reunion'),
+(182, 'RO', 'Romania'),
+(183, 'RU', 'Russian Federation'),
+(184, 'RW', 'Rwanda'),
+(185, 'KN', 'Saint Kitts and Nevis'),
+(186, 'LC', 'Saint Lucia'),
+(187, 'VC', 'Saint Vincent and the Grenadines'),
+(188, 'WS', 'Samoa'),
+(189, 'SM', 'San Marino'),
+(190, 'ST', 'Sao Tome and Principe'),
+(191, 'SA', 'Saudi Arabia'),
+(192, 'SN', 'Senegal'),
+(193, 'RS', 'Serbia'),
+(194, 'SC', 'Seychelles'),
+(195, 'SL', 'Sierra Leone'),
+(196, 'SG', 'Singapore'),
+(197, 'SK', 'Slovakia'),
+(198, 'SI', 'Slovenia'),
+(199, 'SB', 'Solomon Islands'),
+(200, 'SO', 'Somalia'),
+(201, 'ZA', 'South Africa'),
+(202, 'GS', 'South Georgia South Sandwich Islands'),
+(203, 'SS', 'South Sudan'),
+(204, 'ES', 'Spain'),
+(205, 'LK', 'Sri Lanka'),
+(206, 'SH', 'St. Helena'),
+(207, 'PM', 'St. Pierre and Miquelon'),
+(208, 'SD', 'Sudan'),
+(209, 'SR', 'Suriname'),
+(210, 'SJ', 'Svalbard and Jan Mayen Islands'),
+(211, 'SZ', 'Eswatini'),
+(212, 'SE', 'Sweden'),
+(213, 'CH', 'Switzerland'),
+(214, 'SY', 'Syrian Arab Republic'),
+(215, 'TW', 'Taiwan'),
+(216, 'TJ', 'Tajikistan'),
+(217, 'TZ', 'Tanzania, United Republic of'),
+(218, 'TH', 'Thailand'),
+(219, 'TG', 'Togo'),
+(220, 'TK', 'Tokelau'),
+(221, 'TO', 'Tonga'),
+(222, 'TT', 'Trinidad and Tobago'),
+(223, 'TN', 'Tunisia'),
+(224, 'TR', 'Turkey'),
+(225, 'TM', 'Turkmenistan'),
+(226, 'TC', 'Turks and Caicos Islands'),
+(227, 'TV', 'Tuvalu'),
+(228, 'UG', 'Uganda'),
+(229, 'UA', 'Ukraine'),
+(230, 'AE', 'United Arab Emirates'),
+(231, 'GB', 'United Kingdom'),
+(232, 'US', 'United States'),
+(233, 'UM', 'United States minor outlying islands'),
+(234, 'UY', 'Uruguay'),
+(235, 'UZ', 'Uzbekistan'),
+(236, 'VU', 'Vanuatu'),
+(237, 'VA', 'Vatican City State'),
+(238, 'VE', 'Venezuela'),
+(239, 'VN', 'Vietnam'),
+(240, 'VG', 'Virgin Islands (British)'),
+(241, 'VI', 'Virgin Islands (U.S.)'),
+(242, 'WF', 'Wallis and Futuna Islands'),
+(243, 'EH', 'Western Sahara'),
+(244, 'YE', 'Yemen'),
+(245, 'ZM', 'Zambia'),
+(246, 'ZW', 'Zimbabwe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nida_data`
+--
+
+CREATE TABLE `nida_data` (
+  `id` varchar(16) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `gender` varchar(15) DEFAULT NULL,
+  `marital_status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `nida_data`
+--
+
+INSERT INTO `nida_data` (`id`, `firstname`, `last_name`, `date_of_birth`, `gender`, `marital_status`) VALUES
+('1234567890123456', 'Mugabo', 'Claude', '2023-05-07', 'MALE', 'SINGLE'),
+('1234567890123457', 'YAMINKIZA', 'Hassan', '2000-05-07', 'MALE', 'SINGLE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `setting_id` int(11) NOT NULL,
+  `path` varchar(300) DEFAULT NULL,
+  `domain` varchar(50) DEFAULT NULL,
+  `document_path` varchar(300) DEFAULT NULL,
+  `aduser` varchar(30) DEFAULT NULL,
+  `adpassword` varchar(32) DEFAULT NULL,
+  `defaultou` varchar(30) DEFAULT NULL,
+  `additionalou` varchar(40) DEFAULT NULL,
+  `mailsenderid` varchar(30) DEFAULT NULL,
+  `mailsendername` varchar(40) DEFAULT NULL,
+  `bulk_sms_url` text DEFAULT NULL,
+  `base_url` text DEFAULT NULL,
+  `app_title` varchar(650) DEFAULT NULL,
+  `app_footer` varchar(100) DEFAULT NULL,
+  `max_sms_trial` int(11) DEFAULT NULL,
+  `max_users` int(11) DEFAULT NULL,
+  `max_email` int(11) DEFAULT NULL,
+  `mail_host` varchar(30) DEFAULT NULL,
+  `app_moto` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`setting_id`, `path`, `domain`, `document_path`, `aduser`, `adpassword`, `defaultou`, `additionalou`, `mailsenderid`, `mailsendername`, `bulk_sms_url`, `base_url`, `app_title`, `app_footer`, `max_sms_trial`, `max_users`, `max_email`, `mail_host`, `app_moto`) VALUES
+(1, 'C:\\Users\\DELL\\Documents\\NetBeansProjects\\company_z\\web', 'companyz.com', 'C:photo', '-', '-', '8', '8', 'info@companyz.com', '-', NULL, 'http://localhost:8084/company_z/', 'Company Z', '  <a href=\"https://companyz.com/\">All Right Reserved, Powered By Akili Heritier</a>', NULL, NULL, NULL, 'mail.companyz.com', '<u><i style=color:green;background-color:white>User Account Management System</i></u>');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` varchar(36) NOT NULL,
+  `names` varchar(150) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `marital_status` varchar(25) NOT NULL,
+  `nationality` varchar(45) DEFAULT NULL,
+  `national_id` varchar(16) DEFAULT NULL,
+  `account_status` varchar(30) DEFAULT NULL,
+  `photo_id` text DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `names`, `gender`, `date_of_birth`, `marital_status`, `nationality`, `national_id`, `account_status`, `photo_id`, `username`, `password`, `role`) VALUES
+('262833eb-372c-4617-b476-91eea2943568', 'YAMINKIZA Hassan', 'MALE', '2000-05-07', 'SINGLE', 'Rwanda', '1234567890123457', 'UNVERIFIED', 'Default.png', 'heritierakili@gmail.com', '1g3ce53fg1b2eb8c77267ee2f57aj5', 'USER'),
+('548c6bc0-44c5-4d70-9a81-18d4ee55f1ef', 'Akili Heritier', 'MALE', '1990-05-07', 'SINGLE', 'Rwanda', '1234567890123455', 'VERIFIED', 'Default.png', 'system@gmail.com', '1g3ce53fg1b2eb8c77267ee2f57aj5', 'ADMIN'),
+('886a0c9c-0523-46f7-b174-e9e99910c1ea', 'Muhire Yves', 'MALE', '2023-05-06', 'SINGLE', 'Rwanda', '119898007950928', 'VERIFIED', 'Default', 'muhire@gmail.com', '886a0c9c-0523-46f7-b174-e9e99910c1ea', 'USER');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `apps_countries`
+--
+ALTER TABLE `apps_countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nida_data`
+--
+ALTER TABLE `nida_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`setting_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `apps_countries`
+--
+ALTER TABLE `apps_countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=247;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
